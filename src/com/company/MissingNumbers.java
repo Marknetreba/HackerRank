38e9381685c5e859a8fe987dca351759980c9f89
mnetreba <mark.netreba@griddynamics.com>
2018-12-21 16:55:44 +0400

    refactoring

---
+++ src/com/company/MissingNumbers.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MissingNumbers {
+
+    static int[] missingNumbers(int[] arr, int[] brr) {
+
+        
+        return null;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int m = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] brr = new int[m];
+
+        String[] brrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int brrItem = Integer.parseInt(brrItems[i]);
+            brr[i] = brrItem;
+        }
+
+        int[] result = missingNumbers(arr, brr);
+
+        scanner.close();
+    }
+}
